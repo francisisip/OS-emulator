4aa95a4bcaefdfd0be159ecbbdf486757624aa84
Cornars <85560422+Cornars@users.noreply.github.com>
2024-10-11 00:19:44 +0800

    Implement CPUCoreWorker class

--- src/CPUCoreWorker.h
+++ src/CPUCoreWorker.h
@@ -5,17 +5,23 @@
 class CPUCoreWorker 
 {
 public:
+    // initializes the core worker, runs the runCoreWorker command 
     void initialize();
-
+    // sets the current process of the core worker
     void setCurrentProcess(std::shared_ptr<Process> process);
+    // boolean to check if there is a process
     bool hasCurrentProcess();
     
 
 private:
+    // runs the current process assigned
     void runProcess();
+    void runCoreWorker();
+    
     int coreId;
     std::thread coreThread;
     bool running;
+    bool assignedProcess;
     std::shared_ptr<Process> currentProcess; // if empty, hasCurrentProcess is false
 
     int totalCPUTicks = 0; // 1 cpu tick = 1 instruction line
