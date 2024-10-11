e224c67e69708be3c34d2efd8ae3866073c28605
Cornars <85560422+Cornars@users.noreply.github.com>
2024-10-11 13:35:36 +0800

    Change main to test scheduler

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,29 @@
-#include "ConsoleManager.h"
 
+#include "ConsoleManager.h"
+#include "Scheduler.h"
+#include <iostream>
 int main()
 {
-	ConsoleManager::initialize();
+	// ConsoleManager::initialize();
 
-	while (true) {
-		ConsoleManager::getInstance()->run();
-	};
+	// while (true) {
+	// 	ConsoleManager::getInstance()->run();
+	// };
+	Scheduler scheduler;
+	scheduler.initialize();
+	scheduler.initializeCores(4);
+	std::shared_ptr<Process> process1 = std::make_shared<Process>("process 1", 100);
+	std::shared_ptr<Process> process2 = std::make_shared<Process>("process 2", 100);
+	std::shared_ptr<Process> process3 = std::make_shared<Process>("process 3", 100);
+	std::shared_ptr<Process> process4 = std::make_shared<Process>("process 4", 100);
+	
+	scheduler.addProcess(*process1);
+	scheduler.addProcess(*process2);
+	scheduler.addProcess(*process3);
+	scheduler.addProcess(*process4);
+
+	scheduler.runScheduler();
+
 
 	return 0;
 }
