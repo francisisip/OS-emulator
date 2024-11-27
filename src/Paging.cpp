257168e521b4778789fd06b393014101dbf5fad9
Cornars <85560422+Cornars@users.noreply.github.com>
2024-11-27 18:14:46 +0800

    feat: initialize memory and freeFrames

--- src/Paging.cpp
+++ src/Paging.cpp
@@ -6,8 +6,17 @@ Paging::Paging(){
   unsigned int memoryPerFrame = Config::getInstance()->getMemoryPerFrame();
   // Get the total number of frames 
   maxPages = maxMemory / memoryPerFrame;
+  // initialize memory to have these number of pages
+  memory.resize(maxPages, -1);
+  // frame indices that are free
+  for (int i = 0; i < maxPages; i++) freeFrames.push_back(i);
 }
 
 void Paging::initialize(){
   instance = new Paging();
 }
+
+bool allocate(std::shared_ptr<Process> processToAllocate){
+  int pid = processToAllocate->getPId();
+
+} 
--- src/Paging.h
+++ src/Paging.h
@@ -14,10 +14,16 @@ public:
     size_t getMaxPages();
     size_t setMaxPages();
 private:
-    static FlatMemoryAllocator* instance;
+    static Paging* instance;
     size_t maxPages;
     size_t allocatedSize;
     int numFrames;
 
-
+    // Physical memory represented as frames.
+    // -1 means 
+    std::vector<int> memory; 
+    // Map of process ID to page table.
+    std::unordered_map<int, std::unordered_map<int, int>> pageTables; 
+    // List of free frames.
+    std::vector<int> freeFrames; 
 };
